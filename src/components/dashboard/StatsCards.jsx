bef4d29bd27cd1c96677af8a08ce09b85a51c892
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-07-02 18:51:19 +0600

    same

--- src/components/dashboard/StatsCards.jsx
+++ src/components/dashboard/StatsCards.jsx
@@ -90,8 +90,8 @@ const StatsCards = ({ transactions = [], user = { balance: 5420.5 } }) => {
     <Box
       sx={{
         width: "100%",
-        maxWidth: { xs: "100%", sm: "90%", md: "100%" },
-        mx: { xs: "0.1%", sm: 1, md: 1 },
+        maxWidth: { xs: "100%", sm: "100%", md: "100%" },
+        mx: { xs: "0%", sm: 1, md: 1 },
         px: { xs: 1, sm: 2, md: 3 },
         py: { xs: 1, sm: 2, md: 3 },
       }}
@@ -107,7 +107,7 @@ const StatsCards = ({ transactions = [], user = { balance: 5420.5 } }) => {
               }`}
               sx={{
                 p: { xs: 2, sm: 3, md: 4 },
-                width: { xs: 150, sm: "100%", md: "100%", lg: 254 },
+                width: { xs: 140, sm: "100%", md: "100%", lg: 254 },
                 minHeight: { xs: 130, sm: 150, md: 180 },
                 display: "flex",
                 flexDirection: "column",
